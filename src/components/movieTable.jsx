795443adefb613f2f6f54bd70f0e6cbd4f607521
Nitin Garg <nitin.garg@perficient.com>
2021-10-08 10:40:45 -0500

    object destructuring and move movie table to a new component

---
+++ src/components/movieTable.jsx
@@ -0,0 +1,42 @@
+import Like from "./like";
+
+const MovieTable = (props) => {
+  const { moviesCollection, onDeleteClick } = props;
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Stock</th>
+          <th scope="col">Rate</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {moviesCollection.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like />
+            </td>
+            <td>
+              <button
+                onClick={() => onDeleteClick(movie)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MovieTable;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,7 @@ import Pagination from "../common/pagination";
 import { paginate } from "../utils/paginate";
 import { getGenres } from "../services/fakeGenreService";
 import ListGroup from "../common/listGroup";
+import MovieTable from "./movieTable";
 class Movies extends Component {
   state = {
     movies: [],
@@ -26,7 +27,7 @@ class Movies extends Component {
   };
 
   handleSelectGenre = (genre) => {
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   componentDidMount() {
@@ -56,38 +57,10 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {paginatedMovies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <MovieTable
+            moviesCollection={paginatedMovies}
+            onDeleteClick={this.handleDelete}
+          />
           <Pagination
             itemCount={filterByGenre.length}
             pageSize={this.state.pageSize}
