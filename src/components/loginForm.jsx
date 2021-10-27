df7936e1314730ba3fd98190d662cff05bfaa7d4
nitingargprft <nitin.garg@perficient.com>
2021-10-27 01:17:44 -0500

    implement controlled input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: {
+      UserName: "",
+      Password: "",
+    },
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("form Submitted");
-    };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form Submitted");
+  };
 
-    render() { 
-        return <div><h1>Login</h1>
-        <form onSubmit={this.handleSubmit}> 
-            <div className="form-group">
-                <lable htmlFor="username">User Name</lable>
-                <input id="username" type="text" className="form-control" />
-            </div>
-            <div className="form-group">
-                <lable htmlFor="password">Password</lable>
-                <input id="password" type="text" className="form-control" />
-            </div>
-            <button className="btn btn-primary">Login</button>
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <lable htmlFor="username">User Name</lable>
+            <input
+              id="username"
+              type="text"
+              className="form-control"
+              value={this.state.account.UserName}
+              onChange={this.handleChange}
+              name="UserName"
+            />
+          </div>
+          <div className="form-group">
+            <lable htmlFor="password">Password</lable>
+            <input
+              id="password"
+              type="text"
+              className="form-control"
+              value={this.state.account.Password}
+              onChange={this.handleChange}
+              name="Password"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
         </form>
-        </div>;
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default LoginForm;
